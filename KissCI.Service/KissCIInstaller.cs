c3c5100b4a4f01ecaa08eade38ff883d9aff76c8
Justin Greene <justin.j.greene@gmail.com>
2012-09-15 22:12:35 -0500

    fixed installer

--- KissCI.Service/KissCIInstaller.cs
+++ KissCI.Service/KissCIInstaller.cs
@@ -1,38 +1,27 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Configuration.Install;
 using System.Linq;
 using System.ServiceProcess;
+using System.Text;
 
 namespace KissCI.Service
 {
     [RunInstaller(true)]
-    public partial class KissCIInstaller : System.Configuration.Install.Installer
+    public class KissCIInstaller : System.Configuration.Install.Installer
     {
-
-        ServiceInstaller installer;
-        ServiceProcessInstaller processInstaller;
-
         public KissCIInstaller()
         {
-            InitializeComponent();
 
-            processInstaller = new ServiceProcessInstaller();
+            var processInstaller = new ServiceProcessInstaller();
             processInstaller.Account = ServiceAccount.LocalSystem;
-            installer = new ServiceInstaller();
+            var installer = new ServiceInstaller();
 
             installer.StartType = ServiceStartMode.Manual;
             installer.ServiceName = "KissCIWebService";
 
             Installers.Add(installer);
             Installers.Add(processInstaller);
-        }
-
-        private void KissCIWebServiceInstaller_AfterInstall(object sender, InstallEventArgs e)
-        {
-
         }
     }
 }
